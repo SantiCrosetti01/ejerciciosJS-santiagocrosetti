aa7b036bbd4d47272edf5b30ce87f93d7ad02871
SantiCrosetti01 <131173067+SantiCrosetti01@users.noreply.github.com>
2023-04-28 07:50:31 -0300

    ejerciciojs1

---
+++ ejercicios.js
@@ -0,0 +1,16 @@
+let miNombre = "Santiago"
+let miApellido = "Crosetti"
+let miEdad = 21
+let miMascota = "Tito"
+let edadMascota = 2 
+
+console.log (miNombre)
+console.log (miApellido)
+console.log (miEdad)
+console.log (miMascota)
+console.log (edadMascota)
+
+
+const nombreCompleto= ´ $(miNombre) $(miApellido)´
+
+console.log (nombreCompleto)
